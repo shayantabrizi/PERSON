eb91a4401ce84d6de40186a2086c2b49e1b6ff78
Shayan Tabrizi <shayantabrizi@gmail.com>
2018-11-15 15:25:54 +0330

    Initial model of precomputation

--- src/main/java/ir/ac/ut/iis/person/algorithms/aggregate/MyValueSource.java
+++ src/main/java/ir/ac/ut/iis/person/algorithms/aggregate/MyValueSource.java
@@ -5,10 +5,15 @@
  */
 package ir.ac.ut.iis.person.algorithms.aggregate;
 
+import ir.ac.ut.iis.person.hierarchy.GraphNode;
 import ir.ac.ut.iis.person.query.Query;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.lucene.queries.function.ValueSource;
 
 /**
@@ -17,6 +22,7 @@ import org.apache.lucene.queries.function.ValueSource;
  */
 public abstract class MyValueSource extends ValueSource {
 
+    private Thread thread;
     private Map<String, Double> features = null;
 
     public void initialize(Query query) {
@@ -38,7 +44,7 @@ public abstract class MyValueSource extends ValueSource {
     public Map<String, Double> getFeatures() {
         return Collections.unmodifiableMap(features);
     }
-    
+
     @Override
     public boolean equals(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -53,7 +59,34 @@ public abstract class MyValueSource extends ValueSource {
     public String description() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     public abstract String getName();
 
+    public abstract void startPreprocess();
+
+    protected void preprocess(Object synchoronizeObject, GraphNode searcher, double pageRankAlpha) {
+        Set<GraphNode> set = new HashSet<>();
+        set.add(searcher);
+        preprocess(synchoronizeObject, set, pageRankAlpha);
+    }
+
+    protected void preprocess(Object synchoronizeObject, Set<GraphNode> searchers, double pageRankAlpha) {
+        thread = new Thread(() -> {
+            for (GraphNode s : searchers) {
+                synchronized (synchoronizeObject) {
+                    s.getHierarchyNode().selfPPR(pageRankAlpha);
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public void joinPreprocess() {
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MyValueSource.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException();
+        }
+    }
 }
